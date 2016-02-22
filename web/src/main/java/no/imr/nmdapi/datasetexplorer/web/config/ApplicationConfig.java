7d565feb02dc26a8fa15b86b231b8871f1610f8f
TerryHannant <thannant_github@metafu.net>
2016-02-22 14:03:55 +0100

    Add http client bean

--- web/src/main/java/no/imr/nmdapi/datasetexplorer/web/config/ApplicationConfig.java
+++ web/src/main/java/no/imr/nmdapi/datasetexplorer/web/config/ApplicationConfig.java
@@ -1,8 +1,18 @@
 package no.imr.nmdapi.datasetexplorer.web.config;
 
+import java.io.IOException;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
 import org.apache.commons.configuration.reloading.ReloadingStrategy;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HttpContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -28,4 +38,11 @@ public class ApplicationConfig {
         configuration.setReloadingStrategy(reloadingStrategy);
         return configuration;
     }
+    
+    @Bean
+    public org.apache.http.client.HttpClient httpClient() {
+        //Create basic client, in future can expand to handle resource management
+        return  HttpClients.createDefault();
+    }
+    
 }
